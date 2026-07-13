fdcbb26bc1a9af9326103e03af6dd4b49c3b634d
Sumedha Eranda <120088434+sumedhaEranda@users.noreply.github.com>
2026-07-13 10:40:53 +0530

    Update screeshots

--- srilanka-flood-app/vite.config.ts
+++ srilanka-flood-app/vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server: {
-    host: '0.0.0.0', // Allow access from network
-    port: 5173,
-    strictPort: false
-  }
-})
+  plugins: [react()],
+  base :process.env.VITE_BASE_PATH || "/react-vite-deploy/",
+});
 
